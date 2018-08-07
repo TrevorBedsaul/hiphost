2c673a2db6ba95f5f6b031717313e7d7d0035c9b
Trevor Bedsaul <tdb7mw@virginia.edu>
2018-08-06 23:11:22 -0400

    worked on ng build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyArIggXUrJ2uRDhha02RBeVU1Hf596KCmI",
     authDomain: "hiphost-71c19.firebaseapp.com",
