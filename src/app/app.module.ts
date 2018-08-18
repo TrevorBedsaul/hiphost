80d0f04e4af3126fa2604e1c35f46ff356d3b3c5
Trevor Bedsaul <tdb7mw@virginia.edu>
2018-08-17 22:01:16 -0400

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -68,7 +68,8 @@ import { AppSafetyComponent } from './app-safety/app-safety.component';
       },
       {
         path: 'home',
-        component: HomeComponent
+        component: HomeComponent,
+        pathMatch: 'full'
       },
       {
         path: 'terms-and-conditions',
